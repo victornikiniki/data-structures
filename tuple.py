5251f24e11ac3c638d026c0202f938f69ee559f8
victornikiniki <vibhisingh1345@gmail.com>
2026-02-16 10:50:17 +1100

    Tuple indexing and function

---
+++ tuple.py
@@ -0,0 +1,13 @@
+hey = (9, 3, 5)
+
+# print(hey[0]) indexing with tuple
+
+def area_perimeter_sq(side_length):
+    area = side_length * side_length
+    perimeter = side_length * 4
+    return (area, perimeter) # tuple paranthesis optional
+
+result = area_perimeter_sq(5) 
+
+print(f"Area: {result[0]} ") 
+print(f"Perimeter: {result[1]} ")
