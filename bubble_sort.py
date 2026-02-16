963bc6f9b560cba205ceb8f6a86d49a2121ec685
victornikiniki <vibhisingh1345@gmail.com>
2026-02-16 17:12:38 +1100

    reyna bubble sort with some dictionaries

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,16 +1,17 @@
 lst = [2, 3, 4, 2, 5, 6, 7, 2, 9, 10, 11, 45, 23, 87, 2, 3, 5, 6]
 
-def bubble_sort(unsorted_list): 
-    is_sorted = False
+def bubble_sort(reyna):
+    is_sorted = False 
+    indexing_length = len(reyna) - 1
 
     while not is_sorted: 
         is_sorted = True 
 
-        for i in range(0, len(unsorted_list) - 1):
-            if unsorted_list[i] > unsorted_list[i+1]:
-                is_sorted = False
-                unsorted_list[i], unsorted_list[i+1] = unsorted_list[i+1], unsorted_list[i]
+        for i in range(0, indexing_length): 
+            if reyna[i] > reyna[i+1]: 
+                is_sorted = False 
+                reyna[i], reyna[i+1] = reyna[i+1], reyna[i]
 
-    return unsorted_list
+    return reyna
 
 print(bubble_sort(lst))
--- dictionary.py
+++ dictionary.py
@@ -15,3 +15,14 @@ user["relationship status"] = "dating"
 remove_item = user.pop("time")
 
 # print(user)
+
+# print(user.get("salary", "Not found"))
+
+try: 
+    print(user.get("salary"))
+except: 
+    print("Not found")
+
+sorted_dict = sorted(user.items(), key = lambda x: x[1])
+
+print(sorted_dict)
